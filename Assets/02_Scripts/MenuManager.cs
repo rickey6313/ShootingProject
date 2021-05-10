a9b09bf048f45116d6f15bbf3ea3aaad85b6186a
rickey6313 <31339135+rickey6313@users.noreply.github.com>
2021-05-10 23:02:14 +0900

    테스트 버튼 추가
    코인 추가 테스트, Playerprefs 제거 버튼 추가

--- Assets/02_Scripts/MenuManager.cs
+++ Assets/02_Scripts/MenuManager.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Game;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject item;
     public GameObject content;
+    public GameObject addButtonObj;
+    public GameObject ClearButtonObj;
+    public Text coinText;
+    public GameObject coinImage;
 
     private void Start()
     {
@@ -26,12 +31,35 @@ public class MenuManager : MonoBehaviour
             curItem.shipImage.sprite = Resources.Load<Sprite>(ship.GetImageName());
             GetComponent<ScrollRectSnap>().item.Add(obj);
         }
+        if(GameDataScript.instance.GetCoin() == 0)
+        {
+            coinImage.SetActive(false);
+            coinText.gameObject.SetActive(false);
+        }
+        else
+        {
+            coinImage.SetActive(true);
+            coinText.gameObject.SetActive(true);
+            coinText.text = GameDataScript.instance.GetCoin().ToString();
+        }
     }
 
     public void GoGameScene()
     {
         SceneManager.LoadScene("GameScene");
     }
+
+    public void AddTestCoin()
+    {
+        GameDataScript.instance.AddCoin(10000);
+        coinText.text = GameDataScript.instance.GetCoin().ToString();
+    }
+
+    public void ClearPrefAction()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
 
     public void QuitGame()
     {
